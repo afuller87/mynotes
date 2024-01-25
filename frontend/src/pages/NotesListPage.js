ffda349983901670fe63746d4537d907fa64dd51
afuller87 <amyfuller19@gmail.com>
2024-01-25 02:01:20 -0500

    Note Page, Added List Item, Updated Package to add Proxy for API URL

--- frontend/src/pages/NotesListPage.js
+++ frontend/src/pages/NotesListPage.js
@@ -1,14 +1,15 @@
 import React, {useState, useEffect} from 'react'
+import ListItem from '../components/ListItem'
 
 const NotesListPage = () => {
     let [notes, setNotes] = useState([])
-    
+
     useEffect(() => {
         getNotes()
     }, [])
 
     let getNotes = async () => {
-        let response = await fetch('http://127.0.0.1:8000/api/notes/')
+        let response = await fetch('/api/notes/')
         let data = await response.json()
         console.log('DATA:', data)
         setNotes(data)
@@ -16,7 +17,11 @@ const NotesListPage = () => {
 
   return (
     <div>
-      notes
+      <div className="notes-list">
+        {notes.map((note, index) => (
+            <ListItem key={index} note={note} />
+        ))}
+      </div>
     </div>
   )
 }
